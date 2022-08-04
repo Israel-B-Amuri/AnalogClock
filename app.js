cf9745c15893940d67574e5c780d817f141491ad
Biringanine Amuri Israel <92646009+Israel-B-Amuri@users.noreply.github.com>
2022-08-04 11:52:12 +0300

    seconds hand

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
 setInterval(setClock, 1000);
-
-const hourHand = document.querySelector("[data-hour-hand]");
-const minutesHand = document.querySelector("[data-minutes-hand]");
 const secondsHand = document.querySelector("[data-seconds-hand]");
-
 function setClock() {
   const currentDate = new Date();
   const secondsRatio = currentDate.getSeconds() / 60;
